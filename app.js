dbd72b6670392e540584406f3da4bf23608b6ecd
bhumijgupta <bhumijgupta@gmail.com>
2019-04-14 11:02:19 +0530

    Enabled CORS

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-var express = require('express');
-var fs = require('fs');
+var express = require("express");
+var fs = require("fs");
+var cors = require("cors");
 var app = express();
 var port = process.env.PORT || 3000;
 
-jsonFile = fs.readFileSync(__dirname + '/data.json', 'utf-8');
+app.use(cors());
+
+jsonFile = fs.readFileSync(__dirname + "/data.json", "utf-8");
 jsonData = JSON.parse(jsonFile);
 
-app.get("/api/:pincode", function(req,res){
-	var pincode = req.params.pincode;
-	res.send(jsonData[pincode]);
+app.get("/api/:pincode", function(req, res) {
+  var pincode = req.params.pincode;
+  res.send(jsonData[pincode]);
 });
-app.get("*", function(req,res){
-	res.send("<h1>Find details about pincode.</h1><br>Syntax: /api/pincode")
-})
-app.listen(port, function(){
-	console.log("Server listening on port" + port);
+app.get("*", function(req, res) {
+  res.send("<h1>Find details about pincode.</h1><br>Syntax: /api/pincode");
+});
+app.listen(port, function() {
+  console.log("Server listening on port" + port);
 });
