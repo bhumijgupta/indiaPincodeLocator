2d4c66e1570aa3268336fb5a8aa39ecaaadab5d3
bhumijgupta <bhumijgupta@gmail.com>
2019-05-31 10:47:52 +0530

    Added error for bad pincodes

--- app.js
+++ app.js
@@ -9,13 +9,21 @@ app.use(cors());
 jsonFile = fs.readFileSync(__dirname + "/data.json", "utf-8");
 jsonData = JSON.parse(jsonFile);
 
-app.get("/api/:pincode", function(req, res) {
+app.get("/api/:pincode", (req, res) => {
   var pincode = req.params.pincode;
-  res.send(jsonData[pincode]);
+  var result = jsonData[pincode];
+  if (result == undefined) {
+    res.status(400);
+    res.send({
+      'error': 'Incorrect pincode'
+    });
+  } else {
+    res.send(jsonData[pincode]);
+  }
 });
-app.get("*", function(req, res) {
+app.get("*", (req, res) => {
   res.send("<h1>Find details about pincode.</h1><br>Syntax: /api/pincode");
 });
-app.listen(port, function() {
-  console.log("Server listening on port" + port);
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
